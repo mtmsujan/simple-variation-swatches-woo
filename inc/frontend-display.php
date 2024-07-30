6a1c668cddc329ff8f4338eabd00a3c8d02b245f
mtmsujon <mtmsujon@gmail.com>
2024-07-30 10:25:07 +0600

    display color term value on frontent without style

--- inc/frontend-display.php
+++ inc/frontend-display.php
@@ -2,15 +2,11 @@
 
 class Frontend_Display {
 
-    private $attribute_type;
-    private $taxonomy;
-
     public function __construct() {
         $this->setup_hooks();
     }
 
     public function setup_hooks() {
-        // add_action( 'hook_name', [ $this, 'callback_function_name' ] );
         add_filter( 'woocommerce_dropdown_variation_attribute_options_html', [ $this, 'custom_variation_attribute_options_html' ], 10, 2 );
     }
 
@@ -38,50 +34,69 @@ class Frontend_Display {
         // Get attribute type
         $attribute_type = $this->get_attr_type_by_name( $args['attribute'] );
 
-        // Check if the attribute is of type color
-        if ( $attribute_type !== 'color' ) {
-            return $html;
-        }
-
         // Get the terms for the attribute
         $terms = get_terms( $args['attribute'], array( 'hide_empty' => false ) );
 
-        // Build the HTML for the color swatches
-        /* $output = '<div class="color-swatches">';
-        foreach ( $terms as $term ) {
-            $term_color = get_term_meta( $term->term_id, 'svsw_color', true );
-            $output .= '<span class="color-swatch" data-value="' . esc_attr( $term->slug ) . '" style="background-color: ' . esc_attr( $term_color ) . '"></span>';
-        }
-        $output .= '</div>';
-
-        return $output; */
-    }
-
-    public function put_program_logs( $data ) {
-
-        // Ensure directory exists to store response data
-        $directory = SVSW_PLUGIN_PATH . '/program_logs/';
-        if ( !file_exists( $directory ) ) {
-            mkdir( $directory, 0777, true );
-        }
-
-        // Construct file path for response data
-        $file_name = $directory . 'program_logs.log';
-
-        // Get the current date and time
-        $current_datetime = date( 'Y-m-d H:i:s' );
-
-        // Append current date and time to the response data
-        $data = $data . ' - ' . $current_datetime;
-
-        // Append new response data to the existing file
-        if ( file_put_contents( $file_name, $data . "\n\n", FILE_APPEND | LOCK_EX ) !== false ) {
-            return "Data appended to file successfully.";
+        if ( $attribute_type === 'color' ) {
+            $html = '<div class="color-swatches">';
+            foreach ( $terms as $term ) {
+                $color = get_term_meta( $term->term_id, 'svsw_color', true ); // Assuming color is saved as term meta
+                $html .= sprintf(
+                    '<label class="color-swatch" style="background-color: %s;">
+                        <input type="radio" name="%s" value="%s">
+                        %s
+                    </label>',
+                    esc_attr( $color ),
+                    esc_attr( $args['attribute'] ),
+                    esc_attr( $term->slug ),
+                    esc_html( $term->name )
+                );
+            }
+            $html .= '</div>';
         } else {
-            return "Failed to append data to file.";
+            $html = '<div class="label-swatches">';
+            foreach ( $terms as $term ) {
+                $html .= sprintf(
+                    '<label class="label-swatch">
+                        <input type="radio" name="%s" value="%s">
+                        %s
+                    </label>',
+                    esc_attr( $args['attribute'] ),
+                    esc_attr( $term->slug ),
+                    esc_html( $term->name )
+                );
+            }
+            $html .= '</div>';
         }
+
+        return $html;
     }
 }
 
 new Frontend_Display();
 
+
+/*public function put_program_logs( $data ) {
+
+    // Ensure directory exists to store response data
+    $directory = SVSW_PLUGIN_PATH . '/program_logs/';
+    if ( !file_exists( $directory ) ) {
+        mkdir( $directory, 0777, true );
+    }
+
+    // Construct file path for response data
+    $file_name = $directory . 'program_logs.log';
+
+    // Get the current date and time
+    $current_datetime = date( 'Y-m-d H:i:s' );
+
+    // Append current date and time to the response data
+    $data = $data . ' - ' . $current_datetime;
+
+    // Append new response data to the existing file
+    if ( file_put_contents( $file_name, $data . "\n\n", FILE_APPEND | LOCK_EX ) !== false ) {
+        return "Data appended to file successfully.";
+    } else {
+        return "Failed to append data to file.";
+    }
+}*/
